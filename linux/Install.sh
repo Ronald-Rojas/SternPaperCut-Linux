bcb044e773c9c539811183479fe50674403448a7
RonZenUbu <ronladred@gmail.com>
2015-11-17 17:02:55 -0500

    Finished Installation shell script and created zipped file

--- linux/Install.sh
+++ linux/Install.sh
@@ -1,7 +1,10 @@
- #!/bin/sh
-# run pc-client-linux.sh
-#run the command 
-	#lpadmin -Print@Stern -v "lpd://papercut.stern.nyu.edu/print@stern"
+#!/bin/sh
+#create Stern Network Printer
+sudo lpadmin -p Print@Stern -v "lpd://papercut.stern.nyu.edu/print@stern"
+#run createDesktop to create desktop application link
+sudo ./createDesktop.sh
+#star the papercut client
+sudo ./pc-client-linux.sh
 
 
 
--- linux/script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-echo "it worked"
-sudo ./"pc-client-linux.sh"
-
