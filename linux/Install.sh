8a1b370967b54d4fc76f7b8ec7e900359cb53501
RonZenUbu <ronladred@gmail.com>
2015-11-16 21:15:06 -0500

    The application works!

---
+++ linux/Install.sh
@@ -0,0 +1,7 @@
+ #!/bin/sh
+# run pc-client-linux.sh
+#run the command 
+	#lpadmin -Print@Stern -v "lpd://papercut.stern.nyu.edu/print@stern"
+
+
+
---
+++ linux/script.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+echo "it worked"
+sudo ./"pc-client-linux.sh"
+
